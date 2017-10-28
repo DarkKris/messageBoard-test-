5bb3ff5cdd359c44afc7b782dcb65e26629b5b5d
DarkKris <long4664030@163.com>
2017-10-28 23:56:17 +0800

    fix datebase ,connect datebase and complete Users moduel

--- tp5/application/Home/model/Users.php
+++ tp5/application/Home/model/Users.php
@@ -17,9 +17,17 @@
             return $this->hasMany('comments','UserId','UserId');
         }
         //用户登录验证
+        //  $id 用户名
+        //  $pw 密码
         public function checkUser($id,$pw)
         {
-            return Db::name('users')->where(array('name'=>))
+            return Db::name('users')->where(array('name'=>$id,'password'=>$pw))->find();
+        }
+        //检测用户名是否已经使用过(注册时)
+        //  $username 注册时输入的用户名
+        public function findUser($username)
+        {
+            return Db::name('users')->where(array('name'=>$username))->find();
         }
     }
 ?>
