b112de89d6007687c8422a734c2359a89cf3dd93
DarkKris <long4664030@163.com>
2017-10-31 23:58:55 +0800

    improve directory structure after studying

---
+++ application/index/model/Users.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DarkKris
+ * Date: 2017/10/28
+ * Time: 下午3:32
+ */
+namespace app\index\model;
+use think\Model;
+use think\Db;
+class Users extends Model
+{
+    protected $table='users';//制定当前操作的数据表
+
+    public function comm()
+    {
+        //hasMany('关联模型名','外键名','主键名',['模型别名定义']);
+        //一对多关联users表与comments表
+        return $this->hasMany('comments','UserId','UserId');
+    }
+    //用户登录验证
+    //  $id 用户名
+    //  $pw 密码
+    public function checkUser($id,$pw)
+    {
+        return Db::name('users')->where(array('name'=>$id,'password'=>$pw))->find();
+    }
+    //检测用户名是否已经使用过(注册时)
+    //  $username 注册时输入的用户名
+    public function findUser($username)
+    {
+        return Db::name('users')->where(array('name'=>$username))->find();
+    }
+}
+?>
