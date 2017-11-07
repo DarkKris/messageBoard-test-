77685880a223e6bb546cf3f6aecd03a5be4e243d
DarkKris <long4664030@163.com>
2017-11-07 13:33:40 +0800

    continue buliding avator

--- application/index/controller/Setting.php
+++ application/index/controller/Setting.php
@@ -7,22 +7,35 @@
  */
 namespace app\index\controller;
 use think\Controller;
-use app\index\model\users;
+use app\index\model\Users;
 class Setting extends controller
 {
     public function setting()
     {
-        return $this->fetch('setting/setting');
+        $userssrc=Db::table('users')//从数据库中提取头像信息
+            ->where(array(''));//
+        $this->assign('address',$userssrc);
+        $this->display('setting/setting');
     }
 
     public function upload()
     {
-        
+        $file=request()->file('users.avator');
+
+        $info=$file->move(ROOT_PATH . 'public' . DS . 'uploads');
+        if($info)
+        {
+            $address=$info->getSaveName();
+            //保存至数据库users.imgsrc
+        }else{
+            $this->error('Upload fail');
+        }
     }
 
     public function setpaginate()
     {
         $rows=input('post.number');
+        //保存至数据库users.pagrows
     }
 }
 ?>
